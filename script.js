d899352b824bf90c5e6bcaa4cfd3a2de5e8b34dc
Vince Jones <vincethebutcher@gmail.com>
2015-04-22 00:30:22 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready( function() {
+		$("#getAnimal").on('click', function(e){
+			e.preventDefault(); // stops page from refreshing on button clicks
+			var animal = $("input:radio[name=r]:checked").val(); // get the value of a checked radio button with a name=r
+
+			$("#animalResult").remove();
+			$("#articAnimals").append('<p id="animalResult">' + animal +'</p>');
+		});
+
+		$("input").keyup(function() { // when keys go up start this function to get the value of input
+    			var value = $( this ).val();
+   				 $("#pArea").text( value ); // sets the text area of a selected element
+  				});
+});
