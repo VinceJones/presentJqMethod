b5d725409619cdae23e74fb788a843d9690187bb
Vince Jones <vincethebutcher@gmail.com>
2015-04-22 08:15:05 -0500

    Add picture

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready( function() {
 			var animal = $("input:radio[name=r]:checked").val(); // get the value of a checked radio button with a name=r
 
 			$("#animalResult").remove();
-			$("#articAnimals").append('<p id="animalResult">' + animal +'</p>');
+			$("#articAnimals").append('<p id="animalResult"><img src="walrus.png"/>' + animal +'</p>');
 		});
 
 		$("input").keyup(function() { // when keys go up start this function to get the value of input
